b620e1462041d42be4160057553ff0f116f5b062
Марк <mark@Mark--MacBook-Air.local>
2026-03-19 18:34:30 +0300

    fix android pdf

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,14 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+
+app.use("/uploads", express.static("uploads", {
+  setHeaders: (res, path) => {
+    if (path.endsWith(".pdf")) {
+      res.setHeader("Content-Type", "application/pdf");
+    }
+  }
+}));
 
 // создаем папку если нет
 if (!fs.existsSync("uploads")) {
